b5617e96689847696303c99fdb87b7efe3a1c552
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2023-01-22 11:43:14 +0530

    added model for youtube video

--- youtube/models.py
+++ youtube/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False)
+    published_date = models.DateTimeField("date published", blank=False, null=False)
+    url = models.URLField(blank=False, null=False)
+    thumbnail_url = models.URLField(blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
