71bbb64ccbc83ac43e8807348b1c3c0fc3960ae2
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2023-01-23 10:09:07 +0530

    fixed minor bugs and created migrations

--- youtube/models.py
+++ youtube/models.py
@@ -8,10 +8,10 @@ class Video(models.Model):
     title = models.CharField(max_length=200, blank=False, null=False)
     published_date = models.DateTimeField("date published", blank=False, null=False)
     urls = models.URLField(blank=False, null=False)
-    thumbnail_urls = ArrayField(models.URLField(), blank=False, null=False)
+    # thumbnail_urls = ArrayField(models.URLField(), blank=False, null=False)
     description = models.TextField(blank=False, null=False)
     videoId = models.CharField(max_length=200, blank=False, null=False)
 
 
-class Tag(models.model):
+class Tag(models.Model):
     name = models.CharField(max_length=200, blank=False, null=False)
